a50fe0cffe4d5f315e60abb924e134b79728109d
rodoni <41773436+roodni@users.noreply.github.com>
2019-02-26 20:47:41 +0900

    update 仮UIの参照を取得

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+{
+    // jQuery使え
+    const $ = (q) => document.querySelector(q);
+
+    const
+        $code_input = $("#code_input"),
+        $code_compressed = $("#code_compressed"),
+        $code_swaped = $("#code_swaped"),
+        $run = $("#run"),
+        $bytes_compressed = $("#bytes_compressed"),
+        $bytes_swaped = $("#bytes_swaped");
+
+    
+    
+}
