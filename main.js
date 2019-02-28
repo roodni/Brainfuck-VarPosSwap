4112616b1208e62085c590e69f4f656fe6457bca
rodoni <41773436+roodni@users.noreply.github.com>
2019-02-28 20:00:17 +0900

    add コード圧縮
    命令外文字および+-, <>を消去する関数compressCodeを実装

--- main.js
+++ main.js
@@ -12,6 +12,21 @@
         $bytes_compressed = $("#bytes_compressed"),
         $bytes_swaped = $("#bytes_swaped");
 
-    
+    // 命令外文字および+-,<>を削除
+    const compressCode = (code) => {
+        code = code.replace(/[^+\-><\[\].,]/g, "");
+        const po = /\+-|-\+|><|<>/g
+        while (po.test(code)) {
+            code = code.replace(po, "");
+        }
+        return code;
+    };
+
+    $run.addEventListener("click", () => {
+        const unko = compressCode($code_input.value);
+        $code_compressed.value = unko;
+        //console.clear();
+        //console.log(unko);
+    })
     
 }
