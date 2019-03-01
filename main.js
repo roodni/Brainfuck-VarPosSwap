65f41c05a88b1e022fb21e7c37dd4485f7f7b2d2
rodoni <41773436+roodni@users.noreply.github.com>
2019-03-01 22:40:54 +0900

    add 中間コードからbfコード生成
    中間コードと変数位置テーブルからbfのコードを生成できるように

--- main.js
+++ main.js
@@ -57,12 +57,34 @@
         return chukanCode;
     };
 
-    $run.addEventListener("click", () => {
+    // 中間コードと変数位置テーブルからbfコードを復元
+    // {元コードの変数位置 : 新しい変数位置}
+    // 変数位置の変更がない場合は省略可能
+    const makeCodeFromChukan = (chukan, table) => {
+        let code = "";
+
+        let prevPos = 0;
+        for (let c of chukan) {
+            let pos = (c.pos in table) ? table[c.pos] : c.pos;
+            let move = pos - prevPos;
+            code += ((move >= 0) ? ">" : "<").repeat(Math.abs(move));
+            code += c.code;
+            prevPos = pos;
+        }
+
+        return code;
+    };
+
+    $code_input.addEventListener("change", () => {
         const po = makeChukanCode($code_input.value);
         console.clear();
         for (let s of po) {
             console.log(s);
         }
+        
+        const code = makeCodeFromChukan(po, {});
+        $code_compressed.value = code;
+        $bytes_compressed.innerText = code.length;
     })
     
 }
